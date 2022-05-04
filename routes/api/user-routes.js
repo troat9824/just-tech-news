4048fdc549cb0ed0f3928aea6b0e703dee8d796d
troat9824 <troat9824@gmail.com>
2022-05-04 04:24:54 -0500

    validate password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -46,6 +46,28 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ messaage: 'Incorrect password!'});
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+      });  
+});
+
 router.put('/:id', (req, res) => {
     User.update(req.body, {
         individualHooks: true,
